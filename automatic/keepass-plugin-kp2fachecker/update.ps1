d0b0b7cf45e2b641b9985545c7223773b5d3d442
tiuub <tools-oqd5htxce1@t-online.de>
2021-02-25 15:32:21 +0100

    Inital commit

---
+++ automatic/keepass-plugin-kp2fachecker/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+
+$releases = 'https://github.com/tiuub/KP2faChecker/releases'
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"           #1
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"      #2
+        }
+    }
+}
+
+function global:au_GetLatest {
+     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
+     $regex   = '\.plgx$'
+     $url     = $download_page.links | ? href -match $regex | select -First 1 -expand href #2
+     $url     = 'https://github.com' + $url 
+     $version = $url -split '/' | select -Last 1 -Skip 1
+     $version = $version -replace "v",""#3
+     return @{ Version = $version; URL32 = $url }
+}
+
+update -ChecksumFor 32
